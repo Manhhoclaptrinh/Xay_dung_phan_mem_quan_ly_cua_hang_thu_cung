0e1c4da7814880fbdce609298e73afadedf5e921
playz-pixel <beviet2k5@gmail.com>
2026-05-30 16:43:35 +0700

    staff_model

--- app/admin/models/staff_model.py
+++ app/admin/models/staff_model.py
@@ -9,6 +9,7 @@ class Staff(db.Model):
     role      = db.Column(db.String(80))
     phone     = db.Column(db.String(20))
     shift     = db.Column(db.String(60))
+    email     = db.Column(db.String(120))
     work_days = db.Column(db.Integer, default=0)
     sales     = db.Column(db.BigInteger, default=0)
     color     = db.Column(db.String(10))
@@ -19,6 +20,7 @@ class Staff(db.Model):
             'name':      self.name,
             'role':      self.role,
             'phone':     self.phone,
+            'email':     self.email,
             'shift':     self.shift,
             'work_days': self.work_days,
             'sales':     self.sales,
