07e23685e8c629d8a594f3ce020f9a03cf46f1f5
playz-pixel <beviet2k5@gmail.com>
2026-06-04 00:07:01 +0700

    run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+os.environ['GROQ_API_KEY'] = ''
 from app import create_app
 
 app = create_app()
@@ -5,3 +7,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True, port=5000, use_reloader=True, reloader_type='stat',
             extra_files=[], exclude_patterns=['*.pyc', 'site-packages/*'])
+    
